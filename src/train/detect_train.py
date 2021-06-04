3e65a29c995cda2891881caf42ff82c058edfef8
長田優太郎 <osada090909@icloud.com>
2021-06-04 19:20:04 +0900

    iou

--- src/train/detect_train.py
+++ src/train/detect_train.py
@@ -118,7 +118,7 @@ def detect_model():
 
     model = Model(inputs=vgg16.input, outputs=[x, hidden_1, hidden_2, hidden_3])
     sgd = SGD(learning_rate=1e-4, momentum=0.9)
-    model.compile(loss=loss_func, optimizer=sgd)
+    model.compile(loss=loss_func, optimizer=sgd, metrics=iou())
     #model.compile(loss='mean_squared_error', optimizer=sgd)
 
     return model
