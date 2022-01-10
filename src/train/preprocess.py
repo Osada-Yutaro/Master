2c1a11429455da6ccb5534bfe9c3afd490b7d6ac
長田優太郎 <osada090909@icloud.com>
2022-01-11 04:35:17 +0900

    a

--- src/train/preprocess.py
+++ src/train/preprocess.py
@@ -74,7 +74,7 @@ def load_targets():
             asset_name = json_load['asset']['name']
             asset_name.split('#')
             mov = asset_name[0]
-            print(asset_name[1])
+            print(asset_name)
             time = float(asset_name[1])
 
             frame = int(FPS*time)
