7c0315d5264a30ba7e0124e0b9c157209e08dd10
長田優太郎 <osada090909@icloud.com>
2022-01-11 04:51:45 +0900

    a

--- src/train/preprocess.py
+++ src/train/preprocess.py
@@ -77,6 +77,9 @@ def load_targets():
             time = float(sp[1])
 
             frame = int(FPS*time)
+            print(json_load)
+            print(json_load['regions'])
+            print(json_load['regions']['tags'])
             id = int(json_load['regions']['tags'][0])
             data[frame][id] = (xc, yc)
     return data
