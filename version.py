82a6e57044cf2e604f2eb59c707d229f9a5fc6f7
長田優太郎 <osada090909@icloud.com>
2022-01-11 15:12:50 +0900

    v

---
+++ version.py
@@ -0,0 +1,11 @@
+import os
+import platform
+import tensorflow as tf
+import numpy as np
+import cv2
+
+
+
+print('tf ver:', tf.__version__)
+print('cv ver:', cv2.__version__)
+print('numpy ver:', np.__version__)
