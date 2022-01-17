e45dd26955a87edf99d107e7adc271bf11418783
長田優太郎 <osada090909@icloud.com>
2022-01-17 18:47:20 +0900

    a

--- src/train/re_ID_train.py
+++ src/train/re_ID_train.py
@@ -153,6 +153,8 @@ def main():
                     continue
                 for k in range(TAGS):
                     for x in train_history[j]:
+                        A = det_model.predict(Xs[j])
+                        print(A.shape)
                         y1, y2, y3 = det_model.predict(Xs[j])[0]
 
                         x1, x2, x3 = x
