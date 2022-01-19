4773929f39cc194e12f8452aa8d68e05958475f8
長田優太郎 <osada090909@icloud.com>
2022-01-19 18:33:00 +0900

    a

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
+from tensorflow.keras.models import load_model
 from tensorflow.keras import applications
 from tensorflow.keras.layers import Input
 
-vgg16=applications.vgg16.VGG16(weights='imagenet', include_top=False, input_tensor=Input(shape=(224, 224, 3)))
-vgg16.save('/kw_resources/Master/Model/VGG16')
+vgg16 = load_model('/kw_resources/Master/Model/VGG16')
+
