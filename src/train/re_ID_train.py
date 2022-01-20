32372f1ad723232789e47c54d56e8e8ae12e471e
長田優太郎 <osada090909@icloud.com>
2022-01-20 10:42:58 +0900

    r

--- src/train/re_ID_train.py
+++ src/train/re_ID_train.py
@@ -203,7 +203,7 @@ def main():
                 for k in range(TAGS):
                     target = 1 if k == j else 0.
                     
-                    valid_loss += re_model.evaluate(x=[x1, x2, x3, y1, y2, y3], y=target)
+                    valid_loss += re_model.evaluate(x=[x1, x2, x3, f1, f2, f3], y=target)
                     valid_count += 1.
         
         with open(log_file_path, mode='a') as f:
