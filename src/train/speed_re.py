fe25b7f0616956667ea192de92ef1c2b69571a4d
長田優太郎 <osada090909@icloud.com>
2022-01-25 04:41:28 +0900

    s

---
+++ src/train/speed_re.py
@@ -0,0 +1,116 @@
+import os
+import datetime
+from tensorflow.keras.models import load_model
+from tensorflow.keras.losses import mean_squared_error
+from tensorflow.keras import applications
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, Flatten, BatchNormalization, Conv2D, MaxPooling2D, Concatenate
+import numpy as np
+from tensorflow.python import training
+from preprocess import load_images, load_targets, image_in_frame, point_in_window
+from tensorflow.keras import backend as K
+import cv2
+import time
+from tensorflow.keras.models import load_model, save_model
+
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
+
+"""
+    modelの名前が一致する層を返す
+"""
+def get_layer(model, name):
+    for layer in model.layers:
+        if layer.name == name:
+            return layer
+    return None
+
+def crop(image, center, position, window_size):
+    x, y = position
+    new_center = point_in_window((window_size, window_size), position, center)
+    dst = image[y:y + window_size, x:x + window_size]
+    X = image_in_frame((window_size, window_size), dst)
+    Y = None
+    if new_center is None:
+        Y = [0., 0., 0.]
+    else:
+        xc, yc = new_center
+        Y = [xc, yc, 1.]
+    return X, Y
+
+def load_data(num):
+    X = []
+
+    image = load_images(num)
+    targets = load_targets()[num]
+
+    fgmask = fgbg.apply(np.array(image*255, dtype=np.uint8))
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+
+    HEIGHT, WIDTH, _ = image.shape
+    WIN_SIZE = 192
+
+    for x in range(0, WIDTH - WIN_SIZE, WIN_SIZE//3):
+        for y in range(0, HEIGHT - WIN_SIZE, WIN_SIZE//3):
+            cropped_win = image[y:y + WIN_SIZE, x:x + WIN_SIZE]
+            cropped_win = cv2.resize(cropped_win, (224, 224))
+            cropped_win = np.array([cropped_win])
+            mask = np.array(fgmask[y:y + WIN_SIZE, x:x + WIN_SIZE], np.float32)/255
+            if 0.5 < np.mean(mask):
+                X.append(cropped_win)
+    return np.array(X, dtype=np.float32), 0 == len(targets)
+
+def join_nums(*args):
+    s = ''
+    for x in args:
+        s = s + ' ' + str(x)
+    return s + '\n'
+
+def main():
+    det_model_path = os.path.join(
+        '/Users',
+        'osadayutaro',
+        'Desktop',
+        'Master',
+        'src',
+        'train',
+        'models',
+        'Model',
+        'detModel')
+    det_model = load_model(det_model_path)
+
+    detection_count = 0
+
+    n = 0
+    t = 0
+
+    maxdet = 0
+    maxtime = 0
+
+    histogram = [0 for _ in range(40)]
+
+    for loop in range(1):
+        for i in range(1800):
+            X, isempty = load_data(i)
+            length = len(X)
+            histogram[length] += 1
+            maxdet = max(length, maxdet)
+            n += 1
+            for j in range(length):
+                start = time.time()
+                dst = det_model.predict(X[j])
+                end = time.time()
+                detection_count += 1
+                t += end - start
+                maxtime = max(maxtime, t)
+
+    print('run time:', t, '[sec]')
+    print('run detection:', detection_count)
+    print('frame num:', n)
+    print('max detection:', maxdet)
+    print('max time:', maxtime)
+    print('hist:', histogram)
+    return
+
+if __name__ == '__main__':
+    model = main()
